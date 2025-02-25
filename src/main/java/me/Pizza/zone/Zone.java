fef663537f2c8d47e07164fc4fee1a0559679d07
Pizza <pizzaformc@gmail.com>
2025-02-25 17:35:23 +0700

    555

--- src/main/java/me/Pizza/zone/Zone.java
+++ src/main/java/me/Pizza/zone/Zone.java
@@ -18,12 +18,17 @@ public class Zone {
         Location location = FurnitureShop.getPlugin().getZoneConfig().getViewerLocation();
         player.teleport(location);
         player.sendMessage("Welcome to furniture shop!");
+        preview();
     }
 
     public void exit() {
         Location location = FurnitureShop.getPlugin().getZoneConfig().getExitLocation();
         player.teleport(location);
         player.sendMessage("Goodbye!");
+    }
+
+    public void preview() {
+
     }
 
 }
